aa4659e109467752d7d5d68899994c4a3a91d0b6
Guilherme Saboia <gsaboia@yahoo.com>
2021-07-29 14:39:16 -0300

    Adiciona for/of

--- 01-fundamentos/bloco-04-javascript-logica/dia-02-bloco-04/arrays-for-for-of/script.js
+++ 01-fundamentos/bloco-04-javascript-logica/dia-02-bloco-04/arrays-for-for-of/script.js
@@ -4,14 +4,15 @@ coins.sort();
 coins.push('LUNA');
 coins.unshift('sETH');
 // coins.pop('LUNA');
-let btc = coins[coins.indexOf('BTC')];
 let coinsLength = coins.length;
+let btc = coins[coins.indexOf('BTC')];
 let sol = coins[coins.indexOf('SOL')];
 
 console.log(coins);
 console.log(sol);
 console.log(coinsLength);
 
+// for/of retorna item da lista, não altera o array
 for (let letter of btc) {
     console.log(letter);
 }
@@ -19,11 +20,16 @@ for (let letter of btc) {
 // for (let index = 0; index < coinsLength; index += 1) {
 //     console.log('Moeda:', coins[index]);
 // }
-
 let prices = [2300, 40000, 2310, 20, 30, 12];
+
+for (let price of prices) {
+    price += 1;
+    console.log(price);
+}
+
 for (let coin of coins) {
     let price = prices[coins.indexOf(coin)];
-        console.log('Moeda:', coin, '\nPrice:', price);
+        console.log('Coin:', coin, '\nPrice:', price);
 }
 
 
