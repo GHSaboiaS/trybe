7ea222a87a6d43a551302b8d8fdd3fe8aee9f338
Guilherme Saboia <gsaboia@yahoo.com>
2021-07-30 16:50:14 -0300

    Adiciona exercicios difíceis 4-3

--- 01-fundamentos/bloco-04-javascript-logica/dia-03-bloco-04/exercicios-4.3/exercicios-4-3.js
+++ 01-fundamentos/bloco-04-javascript-logica/dia-03-bloco-04/exercicios-4.3/exercicios-4-3.js
@@ -17,4 +17,33 @@ function buildTriangle(n) {
         console.log(asterisc.repeat(counter1));
     }
 }
-buildTriangle(5);
+// buildTriangle(5);
+
+// 3- Agora inverta o lado do triângulo.
+function buildInvertedTriangle(n) {
+    for (let counter = 1, asterisc = '*', space = ' '; counter <= n; counter += 1) {
+        process.stdout.write(space.repeat(n - counter) + asterisc.repeat(counter) + '\n');
+    }
+}
+// buildInvertedTriangle(5);
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+function buildPiramid(n) {
+    for (let counter = 1, asterisc = '*', space = ' '; counter <= n; counter += 2) {
+        process.stdout.write(space.repeat((n - counter) / 2) + asterisc.repeat(counter) + space.repeat((n - counter) / 2) + '\n');
+    }
+}
+// buildPiramid(10);
+
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar
+function weirdPiramid(n) {
+    for (let counter = 1, asterisc = '*', space = ' '; counter <= n; counter += 2) {
+        if (counter == 1) {
+            process.stdout.write(space.repeat((n - counter) / 2) + asterisc.repeat(counter) + space.repeat((n - counter) / 2) + '\n');
+        }
+        else {
+            process.stdout.write(space.repeat((counter - 1) / 2)+ asterisc + space.repeat (counter - 2) + asterisc + space.repeat((counter - 1) / 2) + '\n')
+        }
+    }
+}
+weirdPiramid(10);
