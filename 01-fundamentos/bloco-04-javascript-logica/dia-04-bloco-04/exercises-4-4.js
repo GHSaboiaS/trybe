f2b2556d132e6ad3bb998ad1f4a7d241b16a6cba
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-02 17:21:36 -0300

    Adiciona exercise 5

--- 01-fundamentos/bloco-04-javascript-logica/dia-04-bloco-04/exercises-4-4.js
+++ 01-fundamentos/bloco-04-javascript-logica/dia-04-bloco-04/exercises-4-4.js
@@ -1,3 +1,18 @@
+let info = {
+    infoMargarida: {
+        personagem: 'Margarida',
+        origem: 'Pato Donald',
+        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+        recorrente: 'Sim',
+    },
+    infoPatinhas: {
+        personagem: 'Tio Patinhas',
+        origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+        nota: 'O último MacPatinhas',
+        recorrente: 'Sim',
+    }
+}
+
 let infoMargarida = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -27,4 +42,34 @@ for (let key in info) {
 for (let key in info) {
     // console.log(info[key]);
 }
+
+// Exercise 5
+let personagens = [], origens = [], notas = [], recorrentes = [];
+function getInfo() {
+    for (let key in info) {
+        for (let specificKey in info[key]) {
+            if (specificKey === 'personagem') {
+                personagens.push(info[key][specificKey]);
+            } else if (specificKey === 'origem') {
+                origens.push(info[key][specificKey]);
+            } else if (specificKey === 'nota') {
+                notas.push(info[key][specificKey]);
+            } else if (specificKey === 'recorrente') {
+                recorrentes.push(info[key][specificKey]);
+            }
+        }
+    }
+}
+// console.log(personagens)
+for (let counter = 0; counter < Object.keys(info).length; counter += 1) {
+    // console.log(personagens[counter]);
+}
+getInfo(info)
+console.log(personagens[0], 'e', personagens[1]);
+console.log(origens[0], 'e', origens[1]);
+console.log(notas[0], 'e', notas[1]);
+if (recorrentes[0] === recorrentes[1]) {
+    console.log('Ambos recorrentes');
+}
+
 
