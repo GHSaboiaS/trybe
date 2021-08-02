199eb1cb2fa0f0dc181f9a200edb42717d4cdbe1
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-02 16:10:24 -0300

    Atualiza study-4-4

--- 01-fundamentos/bloco-04-javascript-logica/dia-04-bloco-04/study-4-4.js
+++ 01-fundamentos/bloco-04-javascript-logica/dia-04-bloco-04/study-4-4.js
@@ -1,3 +1,4 @@
+// Objetos
 let player = {
     name: 'Marta',
     lastName: 'Silva',
@@ -8,6 +9,47 @@ let player = {
     },
     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 }
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos.');
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita ' + player.bestInTheWorld.length + ' vezes a melhor do mundo.');
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos.');
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita ' + player.bestInTheWorld.length + ' vezes a melhor do mundo.');
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+// Arrays
+let pizzas = {
+    sabor: 'Palmito',
+    preco: 39.90,
+    bordaCatupiry: true,
+}
+
+let pizzasDoces = ['Chocolate', 'Banana', 'Morango'];
+
+// For/in itera nas chaves
+// for (let key in pizzasDoces) {
+//     console.log(key, pizzasDoces[key])
+// }
+
+// // For/of itera nos valores
+// for (let value of pizzasDoces) {
+//     console.log('value:', value);
+// }
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+};
+// for (let name in names) {
+//     console.log('Olá,', names[name])
+// }
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+// delete car['manufacturer']; // delete car.manufacturer
+for (let property in car) {
+    console.log('property:', property, '\nvalue:', car[property]);
+}
+
+let size = Object.keys(car).length
+console.log(size)
