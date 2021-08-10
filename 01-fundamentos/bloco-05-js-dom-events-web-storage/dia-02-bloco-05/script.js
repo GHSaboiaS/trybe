4e842edee64b50f5202d73c8df5e7d145f875049
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-10 15:01:14 -0300

    Adds study 1

---
+++ 01-fundamentos/bloco-05-js-dom-events-web-storage/dia-02-bloco-05/script.js
@@ -0,0 +1,14 @@
+// console.log(document.querySelector('ul').parentNode)
+// console.log(document.querySelector('ul').parentElement)
+
+// console.log(document.querySelector('ol').childNodes)
+// console.log(document.querySelector('ol').children) // returns HTMLCollection
+
+// console.log(document.querySelector('body').firstChild)
+// console.log(document.querySelector('.first').firstElementChild)
+
+// console.log(document.querySelector('.first').nextSibling)
+// console.log(document.querySelector('.first').nextElementSibling)
+
+let ondeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+console.log(ondeVoceEsta)
