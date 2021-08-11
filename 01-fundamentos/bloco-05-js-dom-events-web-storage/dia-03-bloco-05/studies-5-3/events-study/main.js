ae87bab35a6de4ed7dad741d89ee246d26fb75ea
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-11 18:13:09 -0300

    Adds study 4

--- 01-fundamentos/bloco-05-js-dom-events-web-storage/dia-03-bloco-05/studies-5-3/events-study/main.js
+++ 01-fundamentos/bloco-05-js-dom-events-web-storage/dia-03-bloco-05/studies-5-3/events-study/main.js
@@ -54,6 +54,10 @@ input.addEventListener("input", changeTechText);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+function redirect() {
+    window.location.href = "https://ghsaboias.github.io";
+}
+myWebpage.addEventListener("click", redirect);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
