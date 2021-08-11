3411e99e650b6d11a45b1c73b39369a6cf848931
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-11 17:38:23 -0300

    Adds study 2

--- 01-fundamentos/bloco-05-js-dom-events-web-storage/dia-03-bloco-05/studies-5-3/events-study/main.js
+++ 01-fundamentos/bloco-05-js-dom-events-web-storage/dia-03-bloco-05/studies-5-3/events-study/main.js
@@ -4,39 +4,12 @@ const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
-1. Copie esse arquivo e edite apenas ele;
+//
+// 1. Copie esse arquivo e edite apenas ele;
 
-2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
-3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
-
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
-
-5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
-*/
-
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
-
-// 2
+// 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+// Link: https://www.javascripttutorial.net/javascript-dom/javascript-siblings/
 function getSiblings(element) {
     let siblings = [];
     let sibling = element.parentNode.firstChild;
@@ -55,7 +28,6 @@ function getSiblings(element) {
     }
     return siblings;
 }
-
 function addTech(originEvent) {
     let element = originEvent.target;
     let siblings = getSiblings(element);
@@ -70,3 +42,32 @@ function addTech(originEvent) {
 firstLi.addEventListener("click", addTech)
 secondLi.addEventListener("click", addTech)
 thirdLi.addEventListener("click", addTech)
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+function changeTechText() {
+    
+}
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portifólio?
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
+
