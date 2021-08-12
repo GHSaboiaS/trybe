b303c190c83094db29ed2dbe9ae229b275efacf5
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-12 15:21:59 -0300

    Adds studies-5-4

---
+++ 01-fundamentos/bloco-05-js-dom-events-web-storage/dia-04-bloco-05/studies-5-4/app.js
@@ -0,0 +1,42 @@
+document.cookie = "email=gsaboia@yahoo.com; expires=Fri, 13 Aug 2021 12:00:00 UTC";
+let myCookie = document.cookie;
+// console.log(document.cookie);
+// To delete cookie, pass a past date as expiry date
+document.cookie = "email=; expires =Tue, 01 Dec 2020 12:00:00 UTC";
+
+console.log(localStorage.length) // não possui nada salvo, então o retorno é o valor: 0
+localStorage.setItem("firstname", "Adam") //salva uma entrada com a key = "firstname" e value = "Adam"
+localStorage.setItem("lastname", "Smith") //salva uma entrada com a key = "lastname" e value = "Smith"
+console.log(localStorage.getItem("lastname")) // retorna o valor "Smith"
+console.log(localStorage.length) // possui duas entradas, então o retorno é o valor: 2
+localStorage.removeItem("lastname") // remove a entrada referente a key = "lastname"
+console.log(localStorage.length) // possui uma entrada, então o retorno é o valor: 1
+localStorage.clear() // limpa todas as entradas salvas em localStorage
+console.log(localStorage.length) // não possui nada salvo, então o retorno é o valor: 0
+
+console.log(sessionStorage.length) // não possui nada salvo, então o retorno é o valor: 0
+sessionStorage.setItem("firstname", "Adam") //salva uma entrada com a key = "firstname" e value = "Adam"
+sessionStorage.setItem("lastname", "Smith") //salva uma entrada com a key = "lastname" e value = "Smith"
+console.log(sessionStorage.getItem("lastname")) // retorna o valor "Smith"
+console.log(sessionStorage.length) // possui duas entradas, então o retorno é o valor: 2
+sessionStorage.removeItem("lastname") // remove a entrada referente a key = "lastname"
+console.log(sessionStorage.length) // possui uma entrada, então o retorno é o valor: 1
+sessionStorage.clear() // limpa todas as entradas salvas em sessionStorage
+console.log(sessionStorage.length) // não possui nada salvo, então o retorno é o valor: 0
+
+let organization = {
+    name: "trybe",
+    since: 2019
+}
+  
+// objeto "storage" pode ser localStorage ou sessionStorage
+
+localStorage.setItem("trybe", JSON.stringify(organization))
+// console.log(localStorage)
+let org = JSON.parse(localStorage.getItem("trybe"))
+console.log(org) // { name: "trybe", since: 2019 }
+
+let classes = ["2019/set", "2019/oct"]
+localStorage.setItem("classes", JSON.stringify(classes))
+let cls = JSON.parse(localStorage.getItem("classes"))
+console.log(cls) // ["2019/set", "2019/oct"]
