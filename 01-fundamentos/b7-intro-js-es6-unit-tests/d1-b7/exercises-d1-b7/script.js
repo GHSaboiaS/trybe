a45e74f8685f4ba55ed40337d97c9e6c3dc373a9
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-27 18:27:05 -0300

    Adds ternary & arrow function exercise

---
+++ 01-fundamentos/b7-intro-js-es6-unit-tests/d1-b7/exercises-d1-b7/script.js
@@ -0,0 +1,7 @@
+const  testingScope = (escopo) => {
+  escopo ? 
+  console.log(`Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !`) : 
+  console.log(`Não devo ser utilizada fora meu escopo (else)`)
+}
+
+testingScope(true);
