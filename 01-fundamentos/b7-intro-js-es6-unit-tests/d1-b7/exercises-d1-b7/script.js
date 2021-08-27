339861beb5d938c0ffc1d03ac81d6b79afaa9df4
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-27 19:32:55 -0300

    Adds part 1 of exercises

--- 01-fundamentos/b7-intro-js-es6-unit-tests/d1-b7/exercises-d1-b7/script.js
+++ 01-fundamentos/b7-intro-js-es6-unit-tests/d1-b7/exercises-d1-b7/script.js
@@ -1,7 +1,13 @@
 const  testingScope = (escopo) => {
-  escopo ? 
-  console.log(`Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !`) : 
-  console.log(`Não devo ser utilizada fora meu escopo (else)`)
+  escopo ? console.log(`Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !`) : console.log(`Não devo ser utilizada fora meu escopo (else)`)
 }
-
 testingScope(true);
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sorter = array => array.sort( (a, b) => a - b )
+
+// function sorter (array) {
+//   return array.sort( (a, b) => a - b);
+// }
+console.log(sorter(oddsAndEvens));
