d6c57264e0108c6ade58eb285f0fe65af7f755a8
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-02 18:46:51 -0300

    Adds bonus part 2 (final)

--- 01-fundamentos/b8-higher-order-functions-js-es6/d1-b8/exercises-d1-b8/bonus.js
+++ 01-fundamentos/b8-higher-order-functions-js-es6/d1-b8/exercises-d1-b8/bonus.js
@@ -1,12 +1,12 @@
 const mage = {
-  healthPoints: 130,
+  healthPoints: 120,
   intelligence: 45,
   mana: 125,
   damage: undefined,
 };
 
 const warrior = {
-  healthPoints: 200,
+  healthPoints: 150,
   strength: 30,
   weaponDmg: 2,
   damage: undefined,
@@ -56,3 +56,50 @@ const mageCost = () => {
     mageManaUsed: mageManaCalculator(),
   }
 }
+
+const gameActions = {
+  warriorTurn: function (func) {
+    const warriorDamage = func();
+    warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
+  },
+  mageTurn: function (func) {
+    const mageDamage = func().mageDamage;
+    const mageManaCost = func().mageManaUsed;
+    mage.damage = mageDamage;
+    mage.mana -= mageManaCost;
+    dragon.healthPoints -= mageDamage;
+  },
+  dragonTurn: function (func) {
+    const dragonDamage = func();
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  roundResult: function () {
+    console.log(battleMembers);
+    if (warrior.healthPoints <= 0) {
+      console.log('warrior is dead');
+    }
+    if (mage.healthPoints <= 0) {
+      console.log('mage is dead');
+    }
+    if (dragon.healthPoints <= 0) {
+      console.log('DRAGON IS DEAD, WE LIVE FOREVER!')
+    }
+    return battleMembers;
+  },
+};
+
+gameActions.warriorTurn(warriorDamage);
+gameActions.mageTurn(mageCost);
+gameActions.dragonTurn(dragonDamage);
+gameActions.roundResult();
+gameActions.warriorTurn(warriorDamage);
+gameActions.mageTurn(mageCost);
+gameActions.dragonTurn(dragonDamage);
+gameActions.roundResult();
+gameActions.warriorTurn(warriorDamage);
+gameActions.mageTurn(mageCost);
+gameActions.dragonTurn(dragonDamage);
+gameActions.roundResult();
