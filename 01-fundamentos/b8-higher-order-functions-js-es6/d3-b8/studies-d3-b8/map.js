45b614100dadad3386a71674a75972e73e62db20
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-08 15:25:24 -0300

    Adds map

--- 01-fundamentos/b8-higher-order-functions-js-es6/d3-b8/studies-d3-b8/map.js
+++ 01-fundamentos/b8-higher-order-functions-js-es6/d3-b8/studies-d3-b8/map.js
@@ -1,13 +1,32 @@
-const persons = [
+const people = [
   { firstName: 'Maria', lastName: 'Ferreira' },
   { firstName: 'João', lastName: 'Silva' },
   { firstName: 'Antonio', lastName: 'Cabral' },
 ];
 
-const fullNames = [];
+let fullNames = [];
 
-for (let index = 0; index < persons.length; index += 1) {
-  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
-}
+// for (let index = 0; index < people.length; index += 1) {
+//   fullNames.push(`${people[index].firstName} ${people[index].lastName}`);
+// }
 
-console.log(fullNames);
+fullNames = people.map((element) => `${element.firstName} ${element.lastName}`);
+
+// console.log(fullNames);
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts)
