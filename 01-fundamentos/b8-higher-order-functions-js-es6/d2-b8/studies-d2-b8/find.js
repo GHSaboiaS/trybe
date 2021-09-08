bb90d7e6e95c651bc396bd2845a0766c635a9016
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-07 21:09:03 -0300

    Adds allEven

--- 01-fundamentos/b8-higher-order-functions-js-es6/d2-b8/studies-d2-b8/find.js
+++ 01-fundamentos/b8-higher-order-functions-js-es6/d2-b8/studies-d2-b8/find.js
@@ -2,9 +2,11 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const verifyEven = (number) => number % 2 === 0;
 
-const isEven = numbers.find(verifyEven);
+const isEven = numbers.find(verifyEven)
+const allEven = numbers.every(verifyEven)
 
 // console.log(isEven); // 30
+console.log(allEven)
 
 // console.log(verifyEven(9)); // False
 // console.log(verifyEven(14)); // True
