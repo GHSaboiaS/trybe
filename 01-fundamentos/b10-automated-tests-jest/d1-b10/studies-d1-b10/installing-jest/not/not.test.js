f4cfb562cd2e0cef534dcc18db24defb0113505d
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-21 16:35:43 -0300

    Adds Jest tests (toBe, toEqual, toThrow, not

---
+++ 01-fundamentos/b10-automated-tests-jest/d1-b10/studies-d1-b10/installing-jest/not/not.test.js
@@ -0,0 +1,9 @@
+const { weekDays, workDays } = require('./not.js');
+
+test('Sunday is a week day', () => {
+  expect(weekDays).toContain('Sunday');
+});
+
+test('Sunday is not a workday', () => {
+  expect(workDays).not.toContain('Sunday');
+});
