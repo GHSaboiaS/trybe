85993bb0b829739be93bfad6871f1cf6a04d34a4
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-21 20:41:55 -0300

    Renames dirs

---
+++ 01-fundamentos/b10-automated-tests-jest/d1-b10/exercises-d1-b10/part2/tests/decode.test.js
@@ -0,0 +1,13 @@
+const decode = require('./decode.js')
+
+describe('Testing function decode', () => {
+  test('Test if encode is a function', () => {
+    expect(typeof decode).toBe('function')
+  })
+  test('Decode turns vowels into numbers', () => {
+    expect(decode('1bcd2fg')).toEqual('abcdefg')
+  })
+  test('Argument and return strings have same length', () => {
+    expect(decode('abcde').length).toBe('abcde'.length)
+  })
+})
---
+++ 01-fundamentos/b10-automated-tests-jest/d1-b10/exercises-d1-b10/part2/tests/encode.test.js
@@ -0,0 +1,13 @@
+const encode = require('./encode.js')
+
+describe('Testing function encode', () => {
+  test('Test if encode is a function', () => {
+    expect(typeof encode).toBe('function')
+  })
+  test('Encode turns vowels into numbers', () => {
+    expect(encode('abcde1g')).toEqual('1bcd21g')
+  })
+  test('Argument and return strings have same length', () => {
+    expect(encode('abcde').length).toBe('abcde'.length)
+  })
+})
