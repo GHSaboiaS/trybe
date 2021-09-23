3116bb76f6a301fbd4da878e19df62b6c2579acc
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-23 18:12:20 -0300

    Adds ghibli

--- 01-fundamentos/b10-automated-tests-jest/d3-b10/studies-d3-b10/math/mathSpyOn.test.js
+++ 01-fundamentos/b10-automated-tests-jest/d3-b10/studies-d3-b10/math/mathSpyOn.test.js
@@ -1,33 +1,10 @@
 const math = require('./math');
-test("#somar sem async/await", () => {
-  // testando se a função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
-  const mockSomar = jest.spyOn(math, "somar");
 
-  mockSomar(1, 2);
-  expect(mockSomar).toHaveBeenCalled();
-  expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar).toHaveBeenCalledWith(1, 2);
-  expect(mockSomar(1, 2)).resolves.toBe(3);
-  return expect(mockSomar(1, 2)).resolves.toBe(3);
-});
-
-test("#somar com async/await", async () => {
-  // Como alternativa você pode usar async/await, nenhuma é superior a outra. Você pode combinar e apenas depende de qual estilo você acha que torna seus testes mais simples.
-  const mockSomar = jest.spyOn(math, "somar");
-
-  mockSomar(1, 2);
-  expect(mockSomar).toHaveBeenCalled();
-  expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar).toHaveBeenCalledWith(1, 2);
-  await expect(mockSomar(1, 2)).resolves.toBe(3);
-});
-
-// jest.fn().mockImplementation(...)
-test("#somar", () => {
+test("#somar1", () => {
   // testando a implementação original, o mock e o mock resetado
 
   // implementação original
-  expect(math.somar(1, 2)).resolves.toBe(3);
+  // expect(math.somar(1, 2)).resolves.toBe(3);
 
   // criando o mock e substituindo a implementação para uma subtração
   math.somar = jest.fn().mockImplementation((a, b) => a - b);
@@ -38,21 +15,18 @@ test("#somar", () => {
   expect(math.somar).toHaveBeenCalledTimes(2);
   expect(math.somar).toHaveBeenLastCalledWith(5, 1);
 
-  // resetando o mock para antes da substituição da implementação
-  // impossível resetar somar para implementação original, pois jest.fn() sobrescreve a função
+  // resetando o mock
   math.somar.mockReset();
-  // somar agora retorna undefined
   expect(math.somar(1, 2)).toBe(undefined);
   expect(math.somar).toHaveBeenCalledTimes(1);
   expect(math.somar).toHaveBeenLastCalledWith(1, 2);
 });
 
-// jest.spyOn() permite transitar entre simulação e comportamento original
-test("#somar", () => {
+test("#somar2", () => {
   // testando a implementação original, o mock e a restauração da função original
 
   // implementação original
-  expect(math.somar(1, 2)).resolves.toBe(3);
+  // expect(math.somar(1, 2)).resolves.toBe(3);
 
   // criando o mock e substituindo a implementação para uma subtração
   const mockSomar = jest
@@ -60,12 +34,12 @@ test("#somar", () => {
     .mockImplementation((a, b) => a - b);
 
   math.somar(5, 1);
-  expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar(5, 1)).toBe(4);
   expect(mockSomar).toHaveBeenCalledTimes(2);
+  expect(mockSomar(5, 1)).toBe(4);
+  expect(mockSomar).toHaveBeenCalledTimes(3);
   expect(mockSomar).toHaveBeenLastCalledWith(5, 1);
 
   // restaurando a implementação original
   math.somar.mockRestore();
-  expect(math.somar(1, 2)).resolves.toBe(3);
+  // expect(math.somar(1, 2)).resolves.toBe(3);
 });
