64a40076a7b46325ed5e217aa4f1f107258f2f49
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-23 21:36:29 -0300

    Refactors gitFetch

--- 01-fundamentos/b10-automated-tests-jest/d2-b10/exercises-d2-b10/tests/gitFetch.test.js
+++ 01-fundamentos/b10-automated-tests-jest/d2-b10/exercises-d2-b10/tests/gitFetch.test.js
@@ -1,10 +1,13 @@
 const getRepos = require('../src/gitFetch.js')
 
 describe('Checking gitFetch', () => {
-  it('if specific projects are in API return', async () => {
-    const expected = ['sd-01-week4-5-project-todo-list', 'sd-01-week4-5-project-meme-generator']
-    const repos = await getRepos('https://api.github.com/orgs/tryber/repos')
-    const isIncluded = expected.every((el) => repos.includes(el))
-    expect(isIncluded).toBe(true)
+  it('if specific projects are in API return', () => {
+    const url = 'https://api.github.com/orgs/tryber/repos'
+
+    return getRepos(url).then((result) => {
+      expect(result).toContain('sd-01-week4-5-project-todo-list')
+      expect(result).toContain('sd-01-week4-5-project-meme-generator');
+    })
+
   })
 })
