f3584e6a794438ab93cb2b68ce3352bc54d45a08
Guilherme Saboia <gsaboia@yahoo.com>
2021-09-27 17:03:20 -0300

    Retrieves genres

--- spotitrybe/script.js
+++ spotitrybe/script.js
@@ -1,5 +1,6 @@
 const CLIENT_ID = '66db9ace617f4468b94bfe6f4e890412';
 const CLIENT_SECRET = '378ed500fe494a63a75e1a7091ad0da2';
+const BASE_URL = 'https://api.spotify.com/v1';
 
 let token;
 
@@ -20,12 +21,31 @@ async function getToken() {
   const data = await response.json();
 
   token = data.access_token;
+
+  return token;
 }
 
 async function getGenres() {
+  const headers = new Headers({
+    'Authorization': `Bearer ${token}`
+  })
+  const response = await fetch(`${BASE_URL}/browse/categories?country=BR&locale=pt-BR`), {
+    headers
+  }
+  const data = await response.json();
+
+  renderGenres(data.categories.items);
+
+}
+
+// RENDERS
+
+function renderGenres(genres) {
   
 }
 
-window.onload = function() {
-  getToken();
+window.onload = async () => {
+  await getToken();
+
+  getGenres();
 }
