a76a73c3a4ec697d0bdade320d930ea5237b2b5b
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-04 16:10:34 -0300

    Adds react clock

---
+++ 02-front-end/b11-intro-react/d1-b11/studies-d1-b11/src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function tick() {
+  const element = (
+    <div className='blue'>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
