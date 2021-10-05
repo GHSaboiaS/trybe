b11cb50704eedb55a4eb9b53804fc5740d523e2b
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-04 21:42:10 -0300

    Adds studies

---
+++ 02-front-end/b11-intro-react/d1-b11/studies-d1-b11/first-app/src/App.js
@@ -0,0 +1,7 @@
+import FirstComponent from './Component';
+
+function App() {
+  return <FirstComponent />;
+}
+
+export default App;
---
+++ 02-front-end/b11-intro-react/d1-b11/studies-d1-b11/first-app/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FirstComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Guilherme</h1>
+        <p>Eu sou o Mestre dos Magos</p>
+      </div>
+    )}
+}
+
+export default FirstComponent;
