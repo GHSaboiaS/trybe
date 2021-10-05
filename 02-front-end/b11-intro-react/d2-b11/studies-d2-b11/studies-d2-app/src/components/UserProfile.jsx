85a12b55a6fec6b505fe248c86995ed55804bdfe
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-05 18:21:50 -0300

    Adds map inside component

--- 02-front-end/b11-intro-react/d2-b11/studies-d2-b11/studies-d2-app/src/components/UserProfile.jsx
+++ 02-front-end/b11-intro-react/d2-b11/studies-d2-b11/studies-d2-app/src/components/UserProfile.jsx
@@ -3,12 +3,38 @@ import Image from './Image';
 
 class UserProfile extends React.Component {
   render() {
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      },
+
+      {
+        id: 12,
+        name: "Guilherme",
+        email: "guilherme@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      },
+      
+    ]
+
     return (
-      <div>
-        <p> {this.props.user.name} </p>
-        <p> {this.props.user.email} </p>
-        <Image source={this.props.user.avatar} alternativeText='User avatar' />
-      </div>
+      users.map((user) => (
+        <div>
+          <p> {user.name} </p>
+          <p> {user.email} </p>
+          <Image source={user.avatar} alternativeText='User avatar' />
+        </div>
+      ))
     );
   }
 }
