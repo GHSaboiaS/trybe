77a419d66308eb7a37f4e0b5461e9572a88e51f8
Guilherme Saboia <gsaboia@yahoo.com>
2021-11-03 14:58:46 -0300

    Adds tests

--- 02-front-end/b14-react-testing-library/tests-react/src/App.js
+++ 02-front-end/b14-react-testing-library/tests-react/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      inputEmail: '',
+      screenEmail: '',
+    }
+  }
+
+  changeEmail(value) {
+    this.setState({ inputEmail: value });
+  }
+
+  onSendClick(value) {
+    this.setState({ screenEmail: value, inputEmail: '' });
+  }
+
+  render() {
+    const { inputEmail, screenEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+          <input
+            id="id-email"
+            type="email"
+            value={ this.state.inputEmail }
+            onChange={ (e) => this.changeEmail(e.target.value) }
+          />
+        </label>
+        <input
+          id="btn-send"
+          type="button"
+          data-testid="id-send"
+          value="Send"
+          onClick={ () => this.onSendClick(inputEmail) }
+        />
+        <input
+          id="btn-back"
+          type="button"
+          data-testid="id-back"
+          value="Return"
+        />
+        <h1 data-testid="id-email-user">{ screenEmail }</h1>
+      </div>
+    );
+  }
+};
 
 export default App;
--- 02-front-end/b14-react-testing-library/tests-react/src/App.test.js
+++ 02-front-end/b14-react-testing-library/tests-react/src/App.test.js
@@ -1,8 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('runs all tests', () => {
+  it('checks if email input exists', () => {
+    render(<App />);
+    const inputElement = screen.getByLabelText('Email');
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toHaveProperty('type', 'email');
+  });
+  
+  it('checks if two buttons exists', () => {
+    render(<App />);
+    const btns = screen.getAllByRole('button');
+    expect(btns).toHaveLength(2);
+  });
+
+  it('checks if submit button exists', () => {
+    render(<App />);
+    const sendBtn = screen.getByTestId('id-send');
+    expect(sendBtn).toBeInTheDocument();
+    expect(sendBtn).toHaveProperty('type', 'button');
+    expect(sendBtn).toHaveValue('Send');
+  });
+
+  it('checks if return button exists', () => {
+    render(<App />);
+    const returnBtn = screen.getByTestId('id-back');
+    expect(returnBtn).toBeInTheDocument();
+    expect(returnBtn).toHaveProperty('type', 'button');
+    expect(returnBtn).toHaveValue('Return');
+  });
+
+  it('checks if typed email is rendered', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    const sendBtn = screen.getByTestId('id-send');
+    const renderedEmail = screen.getByTestId('id-email-user')
+
+    const USER_EMAIL = 'ghss@test.com';
+
+    userEvent.type(inputEmail, USER_EMAIL);
+    userEvent.click(sendBtn);
+
+    expect(inputEmail).toHaveValue('');
+    expect(renderedEmail).toHaveTextContent(USER_EMAIL);
+  })
+})
