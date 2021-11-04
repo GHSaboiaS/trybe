6a6b42127885469e000ef663cdb51475808883ea
Guilherme Saboia <gsaboia@yahoo.com>
2021-11-04 17:56:24 -0300

    Adds joke fetcher

--- 02-front-end/b14-react-testing-library/rtl-mocks-inputs/src/setupTests.js
+++ 02-front-end/b14-react-testing-library/rtl-mocks-inputs/src/setupTests.js
@@ -4,5 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+// monitor changes in page's DOM
 import MutationObserver from '@sheerun/mutationobserver-shim';
 window.MutationObserver = MutationObserver;
