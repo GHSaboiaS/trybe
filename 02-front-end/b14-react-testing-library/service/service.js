26efd8e53116b52c0dccebe0e7ff41358028d87f
Guilherme Saboia <gsaboia@yahoo.com>
2021-11-05 14:01:07 -0300

    ...

--- 02-front-end/b14-react-testing-library/service/service.js
+++ 02-front-end/b14-react-testing-library/service/service.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const randomNumber = () => Math.floor(Math.random() * 100);
 
 const function1 = (str) => str.toUpperCase();
@@ -6,4 +8,11 @@ const function2 = (str) => str[0];
 
 const function3 = (str1, str2) => str1 + str2;
 
-module.exports = { randomNumber, function1, function2, function3 }
+const getDog = async () => {
+  const URL = 'https://dog.ceo/api/breeds/image/random';
+  const result = await fetch(URL);
+  const resultJSON = await result.json();
+  return resultJSON;
+}
+
+module.exports = { randomNumber, function1, function2, function3, getDog }
